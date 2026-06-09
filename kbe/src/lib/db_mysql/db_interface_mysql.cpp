a76ae1d1b12ecab31716cd7463bc9395b6f77260
IvanDing <ivan.ding95@qq.com>
2026-06-09 20:48:40 +0800

    显式禁用 SSL，避免 MySQL 8.0+ 服务端自动协商 SSL 导致自签名证书错误

--- kbe/src/lib/db_mysql/db_interface_mysql.cpp
+++ kbe/src/lib/db_mysql/db_interface_mysql.cpp
@@ -225,7 +225,9 @@ bool DBInterfaceMysql::attach(const char* databaseName)
 			DEBUG_MSG(fmt::format("DBInterfaceMysql::Enable SSL: sslCert: {}; sslKey:{}; sslCa:{} ...\n", db_mysql_clientKeyPath_, db_mysql_clientCertPath_, db_mysql_caPath_));
 		}
 		else {
-			// 禁用 SSL (MariaDB: default is no SSL)
+			// 显式禁用 SSL，避免 MySQL 8.0+ 服务端自动协商 SSL 导致自签名证书错误
+			int ssl_mode = SSL_MODE_DISABLED;
+			mysql_options(mysql(), MYSQL_OPT_SSL_MODE, &ssl_mode);
 		}
 
 		
